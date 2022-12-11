2ccd626f340694c5ad38e31e0bb7d8215429d322
alexxio1989 <alessiopinna.elis@gmail.com>
2022-12-11 23:57:56 +0100

    avanzamento

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatDrawer } from '@angular/material/sidenav';
+import { SidebarService } from './service/sidebar.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'alessiopinna-fe';
+export class AppComponent implements OnInit{
+  @ViewChild('drawer') drawerComp: MatDrawer;
+
+  showFiller = false;
+
+  openSide = false;
+
+  constructor(private navService: SidebarService) { }
+
+  ngOnInit(): void {
+    this.navService.sbjOpenSide.asObservable().subscribe(next=>{
+      this.openSide = next;
+      if(this.openSide){
+        this.drawerComp.open()
+      } else{
+        this.drawerComp.close()
+      }
+    })
+  }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatDrawer } from '@angular/material/sidenav';
-import { SidebarService } from 'src/app/service/sidebar.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -9,23 +7,9 @@ import { SidebarService } from 'src/app/service/sidebar.service';
 })
 export class HomeComponent implements OnInit {
 
-  @ViewChild('drawer') drawerComp: MatDrawer;
-
-  showFiller = false;
-
-  openSide = false;
-
-  constructor(private navService: SidebarService) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.navService.sbjOpenSide.asObservable().subscribe(next=>{
-      this.openSide = next;
-      if(this.openSide){
-        this.drawerComp.open()
-      } else{
-        this.drawerComp.close()
-      }
-    })
   }
 
 }
