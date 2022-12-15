edbeb4335cff68d191d10ab3737655249c845176
alexxio1989 <alessiopinna.elis@gmail.com>
2022-12-15 22:57:48 +0100

    svil

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -23,10 +23,44 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
     this.isMobile = this.deviceService.isMobile();
     this.slideConfig = {
-      "slidesToShow": this.isMobile ? 1.2 : 3.2,
+      "slidesToShow": 3,
       "slidesToScroll": 1,
       "dots": true,
-      "infinite": false
+      "infinite": false,
+      "centerMode": true,
+      "variableWidth": true,
+      "responsive": [
+        {
+          "breakpoint": 1024,
+          "settings": {
+            "slidesToShow": 3,
+            "slidesToScroll": 3,
+            "centerMode": true,
+            "variableWidth": true,
+            "infinite": true,
+            "dots": true
+          }
+        },
+        {
+          "breakpoint": 600,
+          "settings": {
+            "slidesToShow": 2,
+            "slidesToScroll": 2,
+            "centerMode": true,
+            "variableWidth": true
+          }
+        },
+        {
+          "breakpoint": 480,
+          "settings": {
+            "slidesToShow": 1,
+            "slidesToScroll": 1,
+            "centerMode": true,
+            "variableWidth": true
+          }
+        }
+      ]
+    
     };
     this.corso_service.getCorsi().subscribe(next=>{
       this.corsi = next.corsi;
