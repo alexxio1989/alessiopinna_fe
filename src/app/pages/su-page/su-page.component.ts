767e673f0cd9023dba8bddaa754545016c0f482f
alexxio1989 <alessiopinna.elis@gmail.com>
2022-12-24 10:28:13 +0100

    svil

--- src/app/pages/su-page/su-page.component.ts
+++ src/app/pages/su-page/su-page.component.ts
@@ -27,6 +27,8 @@ export class SuPageComponent implements OnInit {
 
   corsi: Corso[];
 
+  maintab = 0;
+
   constructor(private corso_service: CorsoService , private ds:DelegateService,private route: Router,private user_service:UtenteService,private tpl_service:TlpService) { }
 
   ngOnInit(): void {
@@ -105,6 +107,32 @@ export class SuPageComponent implements OnInit {
     this.corso = new Corso();
     this.corso.imgName = 'default';
     this.corso.prezzo = 0
+  }
+
+  getTab(event:any){
+    //this.login = event.index === 0 ? true : false
+    console.log(event)
+  }
+
+  edit(corso:Corso){
+    this.corso = corso
+    this.maintab = 2
+  }
+
+  save(){
+    this.corso_service.save(this.corso).subscribe(next => {
+      this.corsi = next.corsi;
+      
+      this.ds.sbjSpinner.next(false)
+      if(!next.success){
+        this.ds.sbjErrorsNotification.next(next.error)
+      } else {
+        this.ds.sbjErrorsNotification.next("Salvataggio avvenuto con successo")
+      }
+    },error =>{
+      this.ds.sbjSpinner.next(false)
+      this.ds.sbjErrorsNotification.next("Errore durante il salvataggio")
+    })
   }
 
   
