205e48752afa6c98d88f6e682ee962a26177f696
alexxio1989 <alessiopinna.elis@gmail.com>
2023-01-09 13:05:11 +0100

    svil

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,9 +2,13 @@ import { SocialAuthService } from '@abacritt/angularx-social-login';
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
+import { CalendarEvent } from 'angular-calendar';
 import { DeviceDetectorService } from 'ngx-device-detector';
 import { DialogLoginComponent } from '../dialog/dialog-login/dialog-login.component';
+import { Corso } from '../dto/corso';
+import { EventInfo } from '../dto/EventInfo';
 import { Utente } from '../dto/utente';
+import { getEvent } from '../mapper/calendar-mapper';
 import { SidebarService } from '../service/sidebar.service';
 import { UtenteService } from '../service/utente.service';
 
@@ -20,6 +24,7 @@ export class NavbarComponent implements OnInit {
   userLogged : Utente;
   isUtenteLogged = false;
   isSU = false;
+  mapCorsi = new Map<Corso, CalendarEvent<EventInfo>[]>();
 
   prenotazioniArePresents : boolean
 
@@ -37,6 +42,22 @@ export class NavbarComponent implements OnInit {
       if(this.isUtenteLogged){
         this.prenotazioniArePresents = this.userLogged.prenotazioni && this.userLogged.prenotazioni.length > 0
         this.isSU = 'SU' ===  this.userLogged.tipo.codice
+
+        this.mapCorsi = new Map<Corso, CalendarEvent<EventInfo>[]>();
+
+        if(this.userLogged.prenotazioni && this.userLogged.prenotazioni.length > 0){
+          this.userLogged.prenotazioni .forEach(prenotazione => {
+            const listFiltred = this.mapCorsi.get(prenotazione.corso);
+            if(listFiltred){
+              listFiltred.push(getEvent(prenotazione, true))
+            } else {
+              let newListFiltred = [];
+              newListFiltred.push(getEvent(prenotazione, true))
+              this.mapCorsi.set(prenotazione.corso,newListFiltred);
+            }
+          });
+        }
+        
       } 
     })
 
@@ -46,6 +67,19 @@ export class NavbarComponent implements OnInit {
       this.prenotazioniArePresents = this.userLogged.prenotazioni && this.userLogged.prenotazioni.length > 0
       this.isSU = 'SU' ===  this.userLogged.tipo.codice
     } 
+
+    if(this.userLogged.prenotazioni && this.userLogged.prenotazioni.length > 0){
+      this.userLogged.prenotazioni .forEach(prenotazione => {
+        const listFiltred = this.mapCorsi.get(prenotazione.corso);
+        if(listFiltred){
+          listFiltred.push(getEvent(prenotazione, true))
+        } else {
+          let newListFiltred = [];
+          newListFiltred.push(getEvent(prenotazione, true))
+          this.mapCorsi.set(prenotazione.corso,newListFiltred);
+        }
+      });
+    }
   }
 
   openSideBar(){
